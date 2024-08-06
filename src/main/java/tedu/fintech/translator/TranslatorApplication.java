8a3f5033c6979f04eff4f393ac96570c0b428a36
H4pps <150532087+H4pps@users.noreply.github.com>
2024-08-06 12:41:55 +0200

    add RestTemplate config

--- src/main/java/tedu/fintech/translator/TranslatorApplication.java
+++ src/main/java/tedu/fintech/translator/TranslatorApplication.java
@@ -2,6 +2,7 @@ package tedu.fintech.translator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TranslatorApplication {
@@ -9,5 +10,6 @@ public class TranslatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TranslatorApplication.class, args);
 	}
+
 
 }
